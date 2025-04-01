9cf0b67b2149b48de9d1fe1934f9bcfc2e97cfbe
vadim <vadimworkbox1@gmail.com>
2025-04-01 17:45:27 +0300

    добавлен метод загрузки главной страници

--- automationtests/steps_file.js
+++ automationtests/steps_file.js
@@ -7,7 +7,6 @@ module.exports = function () {
     openMainPage() {
       this.amOnPage(mainPageUrl);
     },
-
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
   });
