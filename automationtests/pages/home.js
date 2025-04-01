e57a6bddc2a7741fc206b97d0de6d1cdab44a25d
vadim <vadimworkbox1@gmail.com>
2025-04-01 17:41:01 +0300

    добавлены локаторы

--- automationtests/pages/home.js
+++ automationtests/pages/home.js
@@ -1,9 +1,10 @@
 const { I } = inject();
 
 module.exports = {
-  myAccountLink: { xpath: '//span[text()="My Account"]' },
-  myAccountRegisterLink: { xpath: '//a[text()="Register"]' },
-//открытие страницы регистрации
+  myAccountLink: { xpath: '//span[text()="My Account"]' }, //локатор ссылки My Account
+  myAccountRegisterLink: { xpath: '//a[text()="Register"]' }, //локатор ссылки Register
+
+  //открытие страницы регистрации
   openRegistrationPage() {
     I.click(this.myAccountLink);
     I.click(this.myAccountRegisterLink);
