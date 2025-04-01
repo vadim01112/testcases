93c28ecc3086b69fbc18d55b2d490ef4823b88c7
vadim <vadimworkbox1@gmail.com>
2025-04-01 13:12:44 +0300

    добавлен комментарий

--- automationtests/pages/home.js
+++ automationtests/pages/home.js
@@ -3,7 +3,7 @@ const { I } = inject();
 module.exports = {
   myAccountLink: { xpath: '//span[text()="My Account"]' },
   myAccountRegisterLink: { xpath: '//a[text()="Register"]' },
-
+//
   openRegistrationPage() {
     I.click(this.myAccountLink);
     I.click(this.myAccountRegisterLink);
