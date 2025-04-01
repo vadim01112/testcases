6d5181b24c32ba006d3ebb9bbe97b8652d17a39b
vadim <vadimworkbox1@gmail.com>
2025-04-01 12:40:46 +0300

    метод загрузки главной страницы

--- automationtests/steps_file.js
+++ automationtests/steps_file.js
@@ -1,10 +1,14 @@
 // in this file you can append custom step methods to 'I' object
 
-module.exports = function() {
+const mainPageUrl = "http://opencart.qatestlab.net/index.php";
+
+module.exports = function () {
   return actor({
+    openMainPage() {
+      this.amOnPage(mainPageUrl);
+    },
 
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
-
   });
-}
+};
